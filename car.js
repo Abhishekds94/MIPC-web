3c594badbebd1fd692cb3e0d5b7ce6ca92014d35
Abhishek D S <abhishekds94@gmail.com>
2020-09-30 23:53:47 -0400

    added db logic skeleton for car

--- car.js
+++ car.js
@@ -2,64 +2,78 @@
 
 //Liability Policy
 
-//motorcycle_upto75
-$('#motorcycle_liability_upto75_btn').click(function(){
-var rootRef = firebase.database().ref().child('motorcycle_liability_upto75');
+//car_upto1000
+$('#car_liability_upto1000_btn').click(function(){
+var rootRef = firebase.database().ref().child('car_liability_upto1000');
 	rootRef.set({		
-		motorcycle_liability_upto75_act:$('#motorcycle_liability_upto75_act').val(),
-		motorcycle_liability_upto75_tax:$('#motorcycle_liability_upto75_tax').val(),
-		motorcycle_liability_upto75_paod:$('#motorcycle_liability_upto75_paod').val()
+		car_liability_upto1000_act:$('#car_liability_upto1000_act').val(),
+		car_liability_upto1000_tax:$('#car_liability_upto1000_tax').val(),
+		car_liability_upto1000_paod:$('#car_liability_upto1000_paod').val()
 	});
-	toastr.success('Values saved!', 'Motorcycle - Upto 75cc (Liability)')
+	toastr.success('Values saved!', 'Car - Upto 1000cc (Liability)')
 })
 
-//motorcycle_upto150
-$('#motorcycle_liability_upto150_btn').click(function(){
-var rootRef = firebase.database().ref().child('motorcycle_liability_upto150');
+//car_upto1500
+$('#car_liability_upto1500_btn').click(function(){
+var rootRef = firebase.database().ref().child('car_liability_upto1500');
 	rootRef.set({		
-		motorcycle_liability_upto150_act:$('#motorcycle_liability_upto150_act').val(),
-		motorcycle_liability_upto150_tax:$('#motorcycle_liability_upto150_tax').val(),
-		motorcycle_liability_upto150_paod:$('#motorcycle_liability_upto150_paod').val()
+		car_liability_upto1500_act:$('#car_liability_upto1500_act').val(),
+		car_liability_upto1500_tax:$('#car_liability_upto1500_tax').val(),
+		car_liability_upto1500_paod:$('#car_liability_upto1500_paod').val()
 	});
-	toastr.success('Values saved!', 'Motorcycle - Upto 150cc (Liability)')
+	toastr.success('Values saved!', 'Car - Upto 1500cc (Liability)')
 })
 
-//motorcycle_upto350
-$('#motorcycle_liability_upto350_btn').click(function(){
-var rootRef = firebase.database().ref().child('motorcycle_liability_upto350');
+//car_above1500
+$('#car_liability_above1500_btn').click(function(){
+var rootRef = firebase.database().ref().child('car_liability_above1500');
 	rootRef.set({		
-		motorcycle_liability_upto350_act:$('#motorcycle_liability_upto350_act').val(),
-		motorcycle_liability_upto350_tax:$('#motorcycle_liability_upto350_tax').val(),
-		motorcycle_liability_upto350_paod:$('#motorcycle_liability_upto350_paod').val()
+		car_liability_above1500_act:$('#car_liability_above1500_act').val(),
+		car_liability_above1500_tax:$('#car_liability_above1500_tax').val(),
+		car_liability_above1500_paod:$('#car_liability_above1500_paod').val()
 	});
-	toastr.success('Values saved!', 'Motorcycle - Upto 350cc (Liability)')
+	toastr.success('Values saved!', 'Car - Above 1500cc (Liability)')
 })
-
-//motorcycle_above350
-$('#motorcycle_liability_above350_btn').click(function(){
-var rootRef = firebase.database().ref().child('motorcycle_liability_above350');
-	rootRef.set({		
-		motorcycle_liability_above350_act:$('#motorcycle_liability_above350_act').val(),
-		motorcycle_liability_above350_tax:$('#motorcycle_liability_above350_tax').val(),
-		motorcycle_liability_above350_paod:$('#motorcycle_liability_above350_paod').val()
-	});
-	toastr.success('Values saved!', 'Motorcycle - Above 350cc (Liability)')
-})
-
 
 //Package Policy
 
-//motorcycle_upto75
-$('#motorcycle_package_upto75_btn').click(function(){
-var rootRef = firebase.database().ref().child('motorcycle_package_upto75');
+//car_upto1000
+$('#car_package_upto1000_btn').click(function(){
+var rootRef = firebase.database().ref().child('car_package_upto1000');
 	rootRef.set({		
-		motorcycle_package_upto75_dop_year1:$('#motorcycle_package_upto75_dop_year1').val(),
-		motorcycle_package_upto75_dop_year2:$('#motorcycle_package_upto75_dop_year2').val(),
-		motorcycle_package_upto75_dop_year3:$('#motorcycle_package_upto75_dop_year3').val(),
-		motorcycle_package_upto75_ndp:$('#motorcycle_package_upto75_ndp').val(),
-		motorcycle_liability_upto75_paod:$('#motorcycle_liability_upto75_paod').val()
+		car_package_upto1000_dop_year1:$('#car_package_upto1000_dop_year1').val(),
+		car_package_upto1000_dop_year2:$('#car_package_upto1000_dop_year2').val(),
+		car_package_upto1000_dop_year3:$('#car_package_upto1000_dop_year3').val(),
+		car_package_upto1000_ndp:$('#car_package_upto1000_ndp').val(),
+		car_package_upto1000_paod:$('#car_package_upto1000_paod').val()
 	});
-	toastr.success('Values saved!', 'Motorcycle - Upto 75cc (Package)')
+	toastr.success('Values saved!', 'Car - Upto 1000cc (Package)')
+})
+
+//car_upto1500
+$('#car_package_upto1500_btn').click(function(){
+var rootRef = firebase.database().ref().child('car_package_upto1500');
+	rootRef.set({		
+		car_package_upto1500_dop_year1:$('#car_package_upto1500_dop_year1').val(),
+		car_package_upto1500_dop_year2:$('#car_package_upto1500_dop_year2').val(),
+		car_package_upto1500_dop_year3:$('#car_package_upto1500_dop_year3').val(),
+		car_package_upto1500_ndp:$('#car_package_upto1500_ndp').val(),
+		car_package_upto1500_paod:$('#car_package_upto1500_paod').val()
+	});
+	toastr.success('Values saved!', 'Car - Upto 1500cc (Package)')
+})
+
+//car_upto1500
+$('#car_package_above1500_btn').click(function(){
+var rootRef = firebase.database().ref().child('car_package_above1500');
+	rootRef.set({		
+		car_package_above1500_dop_year1:$('#car_package_above1500_dop_year1').val(),
+		car_package_above1500_dop_year2:$('#car_package_above1500_dop_year2').val(),
+		car_package_above1500_dop_year3:$('#car_package_above1500_dop_year3').val(),
+		car_package_above1500_ndp:$('#car_package_above1500_ndp').val(),
+		car_package_above1500_paod:$('#car_package_above1500_paod').val()
+	});
+	toastr.success('Values saved!', 'Car - Above 1500cc (Package)')
 })
 
 
@@ -70,58 +84,101 @@ var rootRef = firebase.database().ref().child('motorcycle_package_upto75');
 
 //Liability Policy
 
-//motorcycle_upto75
+//car_upto1000
 $(document).ready(function() { 
-	var rootRef = firebase.database().ref().child('motorcycle_liability_upto75');
+	var rootRef = firebase.database().ref().child('car_liability_upto1000');
 	rootRef.on('value', function(snapshot){
-			$('#motorcycle_liability_upto75_act').val(snapshot.child('motorcycle_liability_upto75_act').val());
-			$('#motorcycle_liability_upto75_tax').val(snapshot.child('motorcycle_liability_upto75_tax').val());
-			$('#motorcycle_liability_upto75_paod').val(snapshot.child('motorcycle_liability_upto75_paod').val());
+			$('#car_liability_upto1000_act').val(snapshot.child('car_liability_upto1000_act').val());
+			$('#car_liability_upto1000_tax').val(snapshot.child('car_liability_upto1000_tax').val());
+			$('#car_liability_upto1000_paod').val(snapshot.child('car_liability_upto1000_paod').val());
 
-			$("label[for='motorcycle_liability_upto75_act']").addClass('active');
-			$("label[for='motorcycle_liability_upto75_tax']").addClass('active');
-			$("label[for='motorcycle_liability_upto75_paod']").addClass('active');
+			$("label[for='car_liability_upto1000_act']").addClass('active');
+			$("label[for='car_liability_upto1000_tax']").addClass('active');
+			$("label[for='car_liability_upto1000_paod']").addClass('active');
 		})
 });
 
-//motorcycle_upto150
+//car_upto1500
 $(document).ready(function() { 
-	var rootRef = firebase.database().ref().child('motorcycle_liability_upto150');
+	var rootRef = firebase.database().ref().child('car_liability_upto1500');
 	rootRef.on('value', function(snapshot){
-			$('#motorcycle_liability_upto150_act').val(snapshot.child('motorcycle_liability_upto150_act').val());
-			$('#motorcycle_liability_upto150_tax').val(snapshot.child('motorcycle_liability_upto150_tax').val());
-			$('#motorcycle_liability_upto150_paod').val(snapshot.child('motorcycle_liability_upto150_paod').val());
+			$('#car_liability_upto1500_act').val(snapshot.child('car_liability_upto1500_act').val());
+			$('#car_liability_upto1500_tax').val(snapshot.child('car_liability_upto1500_tax').val());
+			$('#car_liability_upto1500_paod').val(snapshot.child('car_liability_upto1500_paod').val());
 
-			$("label[for='motorcycle_liability_upto150_act']").addClass('active');
-			$("label[for='motorcycle_liability_upto150_tax']").addClass('active');
-			$("label[for='motorcycle_liability_upto150_paod']").addClass('active');
+			$("label[for='car_liability_upto1500_act']").addClass('active');
+			$("label[for='car_liability_upto1500_tax']").addClass('active');
+			$("label[for='car_liability_upto1500_paod']").addClass('active');
 		})
 });
 
-//motorcycle_upto350
+//car_above1500
 $(document).ready(function() { 
-	var rootRef = firebase.database().ref().child('motorcycle_liability_upto350');
+	var rootRef = firebase.database().ref().child('car_liability_above1500');
 	rootRef.on('value', function(snapshot){
-			$('#motorcycle_liability_upto350_act').val(snapshot.child('motorcycle_liability_upto350_act').val());
-			$('#motorcycle_liability_upto350_tax').val(snapshot.child('motorcycle_liability_upto350_tax').val());
-			$('#motorcycle_liability_upto350_paod').val(snapshot.child('motorcycle_liability_upto350_paod').val());
+			$('#car_liability_above1500_act').val(snapshot.child('car_liability_above1500_act').val());
+			$('#car_liability_above1500_tax').val(snapshot.child('car_liability_above1500_tax').val());
+			$('#car_liability_above1500_paod').val(snapshot.child('car_liability_above1500_paod').val());
 
-			$("label[for='motorcycle_liability_upto350_act']").addClass('active');
-			$("label[for='motorcycle_liability_upto350_tax']").addClass('active');
-			$("label[for='motorcycle_liability_upto350_paod']").addClass('active');
+			$("label[for='car_liability_above1500_act']").addClass('active');
+			$("label[for='car_liability_above1500_tax']").addClass('active');
+			$("label[for='car_liability_above1500_paod']").addClass('active');
 		})
 });
 
-//motorcycle_above350
-$(document).ready(function() { 
-	var rootRef = firebase.database().ref().child('motorcycle_liability_above350');
-	rootRef.on('value', function(snapshot){
-			$('#motorcycle_liability_above350_act').val(snapshot.child('motorcycle_liability_above350_act').val());
-			$('#motorcycle_liability_above350_tax').val(snapshot.child('motorcycle_liability_above350_tax').val());
-			$('#motorcycle_liability_above350_paod').val(snapshot.child('motorcycle_liability_above350_paod').val());
 
-			$("label[for='motorcycle_liability_above350_act']").addClass('active');
-			$("label[for='motorcycle_liability_above350_tax']").addClass('active');
-			$("label[for='motorcycle_liability_above350_paod']").addClass('active');
+//Package Policy
+
+//car_upto1000
+$(document).ready(function() { 
+	var rootRef = firebase.database().ref().child('car_package_upto1000');
+	rootRef.on('value', function(snapshot){
+			$('#car_package_upto1000_dop_year1').val(snapshot.child('car_package_upto1000_dop_year1').val());
+			$('#car_package_upto1000_dop_year2').val(snapshot.child('car_package_upto1000_dop_year2').val());
+			$('#car_package_upto1000_dop_year3').val(snapshot.child('car_package_upto1000_dop_year3').val());
+			$('#car_package_upto1000_ndp').val(snapshot.child('car_package_upto1000_ndp').val());
+			$('#car_package_upto1000_paod').val(snapshot.child('car_package_upto1000_paod').val());
+
+			$("label[for='car_package_upto1000_dop_year1']").addClass('active');
+			$("label[for='car_package_upto1000_dop_year2']").addClass('active');
+			$("label[for='car_package_upto1000_dop_year3']").addClass('active');
+			$("label[for='car_package_upto1000_ndp']").addClass('active');
+			$("label[for='car_package_upto1000_paod']").addClass('active');
+		})
+});
+
+//car_upto1500
+$(document).ready(function() { 
+	var rootRef = firebase.database().ref().child('car_package_upto1500');
+	rootRef.on('value', function(snapshot){
+			$('#car_package_upto1500_dop_year1').val(snapshot.child('car_package_upto1500_dop_year1').val());
+			$('#car_package_upto1500_dop_year2').val(snapshot.child('car_package_upto1500_dop_year2').val());
+			$('#car_package_upto1500_dop_year3').val(snapshot.child('car_package_upto1500_dop_year3').val());
+			$('#car_package_upto1500_ndp').val(snapshot.child('car_package_upto1500_ndp').val());
+			$('#car_package_upto1500_paod').val(snapshot.child('car_package_upto1500_paod').val());
+
+			$("label[for='car_package_upto1500_dop_year1']").addClass('active');
+			$("label[for='car_package_upto1500_dop_year2']").addClass('active');
+			$("label[for='car_package_upto1500_dop_year3']").addClass('active');
+			$("label[for='car_package_upto1500_ndp']").addClass('active');
+			$("label[for='car_package_upto1500_paod']").addClass('active');
+		})
+});
+
+//car_above1500
+$(document).ready(function() { 
+	var rootRef = firebase.database().ref().child('car_package_above1500');
+	rootRef.on('value', function(snapshot){
+			$('#car_package_above1500_dop_year1').val(snapshot.child('car_package_above1500_dop_year1').val());
+			$('#car_package_above1500_dop_year2').val(snapshot.child('car_package_above1500_dop_year2').val());
+			$('#car_package_above1500_dop_year3').val(snapshot.child('car_package_above1500_dop_year3').val());
+			$('#car_package_above1500_ndp').val(snapshot.child('car_package_above1500_ndp').val());
+			$('#car_package_above1500_paod').val(snapshot.child('car_package_above1500_paod').val());
+
+			$("label[for='car_package_above1500_dop_year1']").addClass('active');
+			$("label[for='car_package_above1500_dop_year2']").addClass('active');
+			$("label[for='car_package_above1500_dop_year3']").addClass('active');
+			$("label[for='car_package_above1500_ndp']").addClass('active');
+			$("label[for='car_package_above1500_paod']").addClass('active');
 		})
 });
