8a0d6ef93d8856dcc2408270f65b25ed661d342d
Abhishek D S <abhishekds94@gmail.com>
2020-10-12 21:59:44 -0400

    Bug fixes Goods private

--- goods_private.js
+++ goods_private.js
@@ -54,6 +54,19 @@ var rootRef = firebase.database().ref().child('private_goods_liability_upto40000
 	toastr.success('Values saved!', 'Private Goods - Upto 40000 KGs (Liability)')
 })
 
+//private_goods_above40000
+$('#private_goods_liability_above40000_btn').click(function(){
+var rootRef = firebase.database().ref().child('private_goods_liability_above40000');
+	rootRef.set({		
+		private_goods_liability_above40000_act:$('#private_goods_liability_above40000_act').val(),
+		private_goods_liability_above40000_ll:$('#private_goods_liability_above40000_ll').val(),
+		private_goods_liability_above40000_lpg:$('#private_goods_liability_above40000_lpg').val(),
+		private_goods_liability_above40000_tax:$('#private_goods_liability_above40000_tax').val(),
+		private_goods_liability_above40000_paod:$('#private_goods_liability_above40000_paod').val()
+	});
+	toastr.success('Values saved!', 'Private Goods - Above 40000 KGs (Liability)')
+})
+
 //Package Policy
 
 //private_goods_upto7500
@@ -112,11 +125,11 @@ var rootRef = firebase.database().ref().child('private_goods_package_upto40000')
 $('#private_goods_package_above40000_btn').click(function(){
 var rootRef = firebase.database().ref().child('private_goods_package_above40000');
 	rootRef.set({		
-		private_goods_package_upto20000_dop_year1:$('#private_goods_package_above40000_dop_year1').val(),
-		private_goods_package_upto20000_dop_year2:$('#private_goods_package_above40000_dop_year2').val(),
-		private_goods_package_upto20000_dop_year3:$('#private_goods_package_above40000_dop_year3').val(),
-		private_goods_package_upto20000_ndp:$('#private_goods_package_above40000_ndp').val(),
-		private_goods_package_upto20000_paod:$('#private_goods_package_above40000_paod').val()
+		private_goods_package_above40000_dop_year1:$('#private_goods_package_above40000_dop_year1').val(),
+		private_goods_package_above40000_dop_year2:$('#private_goods_package_above40000_dop_year2').val(),
+		private_goods_package_above40000_dop_year3:$('#private_goods_package_above40000_dop_year3').val(),
+		private_goods_package_above40000_ndp:$('#private_goods_package_above40000_ndp').val(),
+		private_goods_package_above40000_paod:$('#private_goods_package_above40000_paod').val()
 	});
 	toastr.success('Values saved!', 'Private Goods - Above 40000 KGs (Package)')
 })
